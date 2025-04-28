5e90873f18a2854515f3bf00d555bdca4a6f2ae2
fridolin <fridolin_1@yahoo.com>
2025-04-28 17:23:04 +0200

    test - simple ping pong script

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,90 @@
 package de.fridolin1
 
 import com.fazecast.jSerialComm.SerialPort
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 
 
-fun main() {
-    println("starting")
+fun main(args: Array<String>) {
+    println(args.joinToString(prefix = "Arguments: ", separator = ", ") + "\n")
+    if (args[0] == "0") receiver()
+    else if (args[0] == "1") sender()
+    else println("no valid argument: ${args[0]}")
+}
+
+fun sender() {
+    println("starting sender")
     val comPort = SerialPort.getCommPorts()[0]
-    comPort.openPort()
-    println(comPort)
+    if (!comPort.openPort()) {
+        println("Failed to open port!")
+        return
+    }
+    println("Opened port: ${comPort.descriptivePortName}")
     comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0)
-    val inputStream = comPort.inputStream
+
+    val reader = BufferedReader(InputStreamReader(comPort.inputStream))
+    val consoleReader = BufferedReader(InputStreamReader(System.`in`))
+    val writer = BufferedWriter(OutputStreamWriter(comPort.outputStream))
     try {
-        repeat(100000) {
-            print(inputStream.read().toChar())
-            Thread.sleep(10)
+        while (true) {
+            val input = consoleReader.readLine()
+            writer.write(input + "\n")
+            writer.flush()
+            val line = reader.readLine()
+            if (line != null) {
+                println("Received: $line")
+                if (input == "exit")
+                    break
+            } else {
+                println("Stream closed")
+                break
+            }
         }
-        inputStream.close()
     } catch (e: Exception) {
         e.printStackTrace()
+    } finally {
+        reader.close()
+        comPort.closePort()
+        consoleReader.close()
+        writer.close()
+        println("Port closed.")
     }
-    comPort.closePort()
+}
+
+fun receiver() {
+    println("starting receiver")
+    val comPort = SerialPort.getCommPorts()[0]
+    if (!comPort.openPort()) {
+        println("Failed to open port!")
+        return
+    }
+    println("Opened port: ${comPort.descriptivePortName}")
+    comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0)
+
+    val reader = BufferedReader(InputStreamReader(comPort.inputStream))
+    val writer = BufferedWriter(OutputStreamWriter(comPort.outputStream))
+    try {
+        while (true) {
+            val line = reader.readLine()
+            if (line != null) {
+                if (line != "exit") {
+                    println("Received: $line")
+                    writer.write("msg received!\n")
+                    writer.flush()
+                } else break
+            } else {
+                println("Stream closed")
+                break
+            }
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    } finally {
+        reader.close()
+        comPort.closePort()
+        writer.close()
+        println("Port closed.")
+    }
 }
