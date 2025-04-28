48ce536013c71c1b58bb87b622625d2270dd4e38
fridolin <fridolin_1@yahoo.com>
2025-04-28 21:04:19 +0200

    bug fix

--- src/main/kotlin/data/SerializableEntry.kt
+++ src/main/kotlin/data/SerializableEntry.kt
@@ -5,6 +5,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class SerializableEntry(var website: String, var username: String, var password: String, val id: Long = System.nanoTime()) {
     override fun toString(): String {
-        return "{\"website\"=\"$website\",\"username\"=\"$username\",\"id\": $id}"
+        return "{\"website\":\"$website\",\"username\":\"$username\",\"id\": $id}"
     }
 }
--- src/main/kotlin/data/SerializableFolder.kt
+++ src/main/kotlin/data/SerializableFolder.kt
@@ -5,6 +5,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class SerializableFolder(var name: String, var children: List<SerializableFolder>, var entries: MutableList<SerializableEntry>) {
     override fun toString(): String {
-        return "{\"name\"=\"$name\",\"children\"=\"$children\",\"entries\"=\"$entries\"}"
+        return "{\"name\":\"$name\",\"children\":\"$children\",\"entries\":\"$entries\"}"
     }
 }
