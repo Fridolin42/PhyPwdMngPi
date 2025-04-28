b735c532481bd0da3e66d31dd8cb6c57d67cd467
fridolin <fridolin_1@yahoo.com>
2025-04-28 16:40:23 +0200

    test - simple port reader

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,20 @@
 package de.fridolin1
 
 import com.fazecast.jSerialComm.SerialPort
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+import java.util.*
 
 fun main() {
-    println("Hello World!")
-    SerialPort.getCommPorts().forEach { println(it) }
+    val port = SerialPort.getCommPorts()[0]
+    println(port)
+    port.openPort()
+    val scanner = Scanner(port.inputStream)
+    val writer = BufferedWriter(OutputStreamWriter(port.outputStream))
+    while (true) {
+        if (scanner.hasNextLine()) {
+            println(scanner.nextLine())
+            writer.write("Message received")
+        }
+    }
 }
